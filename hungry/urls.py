c6867d8bd3c57b092dbf3730d8fa67cc842ca79b
Yousuf <yalib@gmail.com>
2026-04-13 20:07:45 +0200

    add urls for admin, menu(homepage), orders and accounts

--- hungry/urls.py
+++ hungry/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
 
-from menu.views import index
-
-urlpatterns = [
+urlpatterns= [
     path('admin/', admin.site.urls),
-    path('', index, name="index"),
-    path('accounts/', include('accounts.urls')),
-]
+    path('', include('menu.urls')),
+    path('orders/', include('orders.urls')),
+    path('accounts/', include('accounts.urls')),]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
