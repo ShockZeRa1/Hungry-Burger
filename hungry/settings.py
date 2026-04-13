7be76f7e6d2261e843f2d1d759349e25c8622286
Yousuf <yalib@gmail.com>
2026-04-13 20:16:21 +0200

    base template

--- hungry/settings.py
+++ hungry/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'hungry.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'hungry' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
