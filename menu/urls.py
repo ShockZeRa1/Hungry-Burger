d8c26c600a0cd6917e90259040ac20917ad9997f
Yousuf <yalib@gmail.com>
2026-04-14 01:20:17 +0200

    added html for menu page (homepage) #AI generated HTML

--- menu/urls.py
+++ menu/urls.py
@@ -1,3 +1,5 @@
 from django.urls import path
+from menu import views
 
-urlpatterns= []
+urlpatterns= [path('', views.menu, name='menu'),
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),]
