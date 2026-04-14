22862e33eee484eb4d521a1364fe36a1b493db01
Yousuf <yalib@gmail.com>
2026-04-14 17:16:20 +0200

    make product descriptions textfield instead of charfield

--- menu/models.py
+++ menu/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 # The individual items that are sold
 class Product(models.Model):
     name = models.CharField(max_length=255, unique=True) # Assuming each product has a unique name
-    description = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
     is_featured = models.BooleanField(default=False)
