1f53ab55c50ba007381d1cbf94299b85add8650c
Yousuf <yalib@gmail.com>
2026-04-15 15:07:38 +0200

    fixing import error after merging

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,1 @@
 from django.contrib import admin
-
-from .models import Order, OrderItem, OrderItemOption, OrderStatus
-
-# Register your models here.
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'status', 'total_amount')
-    list_filter = ('status', 'created_at') # Adds a sidebar to filter by "Preparing Order", "Ready", etc.
-
-admin.site.register(Order, OrderAdmin)
-admin.site.register(OrderItem)
-admin.site.register(OrderItemOption)
-admin.site.register(OrderStatus)
