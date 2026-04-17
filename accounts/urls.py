68e3178c922d312fb8c9c993475284408d1731d9
Salman <ms13usa@gmail.com>
2026-04-17 14:44:18 +0200

    Cleared urls.py for accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,4 @@ from . import views
 
 
 urlpatterns = [
-    path("register/", views.register, name="register"),
-    path("login/", views.login, name="login"),
-    path("logout/", views.logout, name="logout"),
-    path("orders/", views.past_orders, name="past_orders"),
-    path("orders<id>/", views.order_detail, name="order_detail"),
 ]
