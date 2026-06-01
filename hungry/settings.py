dde90f5014d542cc1873c286fd6f1a1eb2bbe79a
Muhammad Salman <ms13usa@email.com>
2026-06-01 16:34:43 +0200

    Cleaning settings.py

--- hungry/settings.py
+++ hungry/settings.py
@@ -157,5 +157,4 @@ ACCOUNT_LOGIN_METHODS= {'username'}
 ACCOUNT_SIGNUP_FIELDS= ['username*', 'email*', 'password1*', 'password2*']
 
 ACCOUNT_SIGNUP_FORM_CLASS= 'accounts.forms.SignUpForm'
-# ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'none'
